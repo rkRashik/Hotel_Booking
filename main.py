bb8fda792281f380b3484652dc24f7c47e1d4455
rashik <rkrashik@gmail.com>
2025-07-11 02:53:35 +0600

    Implemented Hotel and ReservationTicket classes to manage hotel reservations and ticket creation.

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
 import pandas
 
-df = pandas.read_csv("hotels.csv")
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
 
 
 class Hotel:
     def __init__(self, hotel_id):
-        pass
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
 
     def book(self):
-        pass
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
-        pass
+        """Check if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def gerenate(self):
-        pass
+        content = f"""
+        Thank you for your reservation!
+        Here is your booking data:
+        Name : {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
+        return content
+
 
 print(df)
 hotel_id = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_id)
+
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(name, hotel)
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
     print(reservation_ticket.gerenate())
 else:
     print("Hotel is not free.")
